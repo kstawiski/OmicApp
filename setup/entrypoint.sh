ef559b4be47dd32c7739f2c16efb5e5f1dfec756
Konrad Stawiski <konrad@konsta.com.pl>
2021-09-12 11:45:14 +0000

    app init

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -9,7 +9,8 @@ then
      echo "Using persistent data application from /home/app directory."
 	else
     echo "Persistent data directory is empty. Initializing app..."
-    
+    wget https://github.com/kstawiski/OmicApp/raw/main/templete.zip && unzip templete.zip -d /home/app && rm templete.zip
+	chown app -R /home/app
 	fi
 else
 	echo "Directory /home/app not found."
